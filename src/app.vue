4475e164fdbf515c6400f63cf589149c7d6f7756
Kirk-Wang <kirk.w.wang@gmail.com>
2020-12-01 20:37:01 +0800

    first commit

---
+++ src/app.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="app">
+        <ul>
+            <li>
+                <router-link to="/about/us">关于我们</router-link>
+                <router-link to="/about/help">帮助</router-link>
+            </li>
+        </ul>
+        <router-view></router-view>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'app',
+    metaInfo() {
+        return {
+            title: '关于我们'
+        };
+    },
+    data() {
+        return {
+            installed: false,
+            show: true
+        };
+    },
+    mounted() {
+        this.installed = true;
+    },
+    methods: {
+        close() {
+            this.show = false;
+        }
+    }
+});
+</script>
+<style lang="less" scoped>
+.app {
+    text-align: center;
+}
+ul {
+    margin: 0;
+    padding: 0;
+}
+li {
+    list-style: none;
+}
+.text {
+    color: #999;
+    font-size: 14px;
+    cursor: pointer;
+}
+</style>
