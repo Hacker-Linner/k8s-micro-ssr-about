82cd539d0b1a09551d5e625c15e889f1d11f83c3
Kirk-Wang <kirk.w.wang@gmail.com>
2020-12-03 17:11:12 +0800

    update

---
+++ ssr-about/src/views/about-help.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        帮助中心 ——
+        <a href="https://k8seggjs.hacker-linner.com/" target="_blank"
+            >互相交流，一起云原生！</a
+        >
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    name: 'about-help'
+});
+</script>
---
+++ ssr-about/src/views/about-us.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        关于我们 ——
+        <a href="https://k8seggjs.hacker-linner.com/" target="_blank"
+            >互相交流，一起云原生！</a
+        >
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    name: 'about-us'
+});
+</script>
