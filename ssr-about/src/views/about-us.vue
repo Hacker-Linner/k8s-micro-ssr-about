85f8fac1fb6a9400cf65a5c289008ec7b2c37968
Kirk-Wang <kirk.w.wang@gmail.com>
2020-12-03 18:35:36 +0800

    update

--- ssr-about/src/views/about-us.vue
+++ ssr-about/src/views/about-us.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h3>我是关于我们页面渲染微服务 —— <a href="https://github.com/Hacker-Linner/k8s-micro-ssr-about" target="_blank">k8s-micro-ssr-about</a></h3>
+        <h3>我是关于我们页面渲染微服务 —— <a href="https://github.com/Hacker-Linner/micro-ssr-about" target="_blank">micro-ssr-about</a></h3>
     </div>
 </template>
 <script lang="ts">
