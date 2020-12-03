6630972cc290de4c2479fce3b044d2077a4a0b91
Kirk-Wang <kirk.w.wang@gmail.com>
2020-12-03 17:10:32 +0800

    update

--- src/views/about-help.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>帮助中心 —— <a href="https://k8seggjs.hacker-linner.com/" target="_blank">互相交流，一起云原生！</a></div>
-</template>
-<script lang="ts">
-import Vue from 'vue';
-export default Vue.extend({
-    name: 'about-help'
-});
-</script>
